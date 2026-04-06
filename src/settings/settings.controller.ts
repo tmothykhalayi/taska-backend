21157bb6296e1daede58502485143b69c1a33e96
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2026-04-06 22:33:54 +0300

    feat(settings): add guards and roles to updateSettings endpoint for access control

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Get, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { SettingsService } from './settings.service';
 import { UpdateSettingsDto } from './dto/update-settings.dto';
+import { AccessTokenGuard } from '../auth/guards/Accesstokenguard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
 
 @Controller('settings')
 export class SettingsController {
@@ -12,6 +16,8 @@ export class SettingsController {
   }
 
   @Patch()
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Roles(Role.ADMIN)
   updateSettings(@Body() updateSettingsDto: UpdateSettingsDto) {
     return this.settingsService.updateSettings(updateSettingsDto);
   }
